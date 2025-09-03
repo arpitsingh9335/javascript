4fd1d9624bd691c6c82b7f368b06650774a1f4e0
arpitsingh9335 <thakur2001111@gmail.com>
2025-09-03 03:52:28 +0000

    Conversion of datatype

---
+++ 02_conversionOperation.js
@@ -0,0 +1,26 @@
+// let score="123"
+
+// console.log(typeof score);
+
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33" =>33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn=1
+let isLogged=""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let booleanIsLogged = Boolean(isLogged)
+console.log(booleanIsLogged);
+
+//1=>true 0=>false
+//""=> false
+//"Arpit" =>true
+
