7a03348e622bad7fee621d256e531542c0bf4a5e
arpitsingh9335 <thakur2001111@gmail.com>
2025-09-03 10:50:41 +0000

    object and json

---
+++ object.js
@@ -0,0 +1,59 @@
+//singleton
+// const tinderUser=new Object()  // it is singleton object
+// console.log(tinderUser);
+//OR => used same
+//  const tanderUser={}      // it is non singlten object
+
+// tanderUser.id="123abc"
+// tanderUser.name="sammy"
+// tanderUser.isLoggedIn = false
+// console.log(tanderUser);
+
+
+
+
+
+
+
+//object literals
+// const jsUser = {
+//     name:"Arpit",
+//     age:22,
+//     location:"lucknow",
+//     email:"arpit@gmail.com",
+//     isLoggedIn:false,
+//     lastLoginDays:["Monday","Saturday"]
+// }
+
+// console.log(jsUser.email)
+// console.log(jsUser["email"])
+
+
+
+
+
+const course={
+    coursename:"javascript",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+
+
+
+const {courseInstructor}=course
+console.log(courseInstructor);
+
+
+// {
+//     "name":"Arpit Singh",
+//     "cousename":"javascript",
+//     "price":"free"
+// }
+
+
+[
+
+    {},
+    {},
+    {}
+]
