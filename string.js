3139c3ad0c16e4e6f653d921579f59c7b7fa78a5
arpitsingh9335 <thakur2001111@gmail.com>
2025-09-03 06:32:28 +0000

    string learning

---
+++ string.js
@@ -0,0 +1,9 @@
+// const name="arpit"
+// const repoCount=50
+
+//console.log(name + repoCount + "value");
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Arpit")
+console.log(gameName);
+console.log(gameName.length);
