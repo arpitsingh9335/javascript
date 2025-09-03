50f55a88383859fb472b8a74a9c4a7074e15bb54
arpitsingh9335 <thakur2001111@gmail.com>
2025-09-03 05:54:10 +0000

    stack and heap

--- datatype-summary.js
+++ datatype-summary.js
@@ -1,13 +1,7 @@
 //primitive datatypes
 // 7 types: String,Number,Boolearn,null,undefined,Symbol,BigInt
 
-const outsideTemp=null
-let userEmail;
 
-const id=Symbol("123")
-const anotherid=Symbol("123")
-
-console.log(id===anotherid);
 
 //const bigNumber = 345687654335666
 
@@ -16,27 +10,53 @@ console.log(id===anotherid);
 
 // Array,object,Function
 
-const heros =["shaktiman" ,"naagraj" ,"Ranapratap"];
-let myObj={
-    name:"Arpit",
-    age:22,
-}
+// const heros =["shaktiman" ,"naagraj" ,"Ranapratap"];
+// let myObj={
+//     name:"Arpit",
+//     age:22,
+// }
+// console.log(myObj.name)
+// console.log(heros[2]);
 
-const myFunction=function(){
-    console.log("hello world");
-}
-function hello(){
-    console.log("Arpit Singh");
+
+// const myFunction=function(){
+//     console.log("hello world");
+// }
+// function hello(){
+//     console.log("Arpit Singh");
     
+// }
+
+
+// console.log(typeof bigNumber);
+
+// myFunction();
+// hello();
+// console.log(typeof myFunction);
+// console.log(typeof hello);
+
+//-----------------------------------------------//
+
+//Stack (Primitive), Heap (Non-Primitive)
+
+// let myName="Arpit Singh"
+
+// let anotherName="shubham"
+// anotherName="monu"
+// console.log(anotherName);
+
+
+let username={
+    email:"arpit@gmail.com",
+    upi:"user@ybl"
 }
 
+let user=username
+user.email="singh@gmail.com"
 
-console.log(typeof bigNumber);
+console.log(username.email);
 
-myFunction();
-hello();
-console.log(typeof myFunction);
-console.log(typeof hello);
+console.log(user.email);
 
 
 
