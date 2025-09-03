be47518a2789cc28b62bde0b002429e5a6c9529a
arpitsingh9335 <thakur2001111@gmail.com>
2025-09-03 02:36:09 +0000

    this is variable & datatype

---
+++ 01_variables.js
@@ -0,0 +1,9 @@
+const accountId=144553
+let accountEmail="thakur2001111@gmail.com"
+var accountPassword="12345"
+accountCity = "jaunpur"
+
+//accountId =2  // not allowed
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+console.log(accountCity);
